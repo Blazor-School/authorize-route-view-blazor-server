d1678c57458691acdc3e3a0eee3b12e55b70a246
Red Wei <hevilhuy2@gmail.com>
2021-08-06 11:29:26 +0700

    Add authorize route view example.

---
+++ AuthorizeRouteViewBlazorServer/Data/LoginFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthorizeRouteViewBlazorServer.Data
+{
+    public class LoginFormModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AuthorizeRouteViewBlazorServer/Data/SimulatedDataProviderService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace AuthorizeRouteViewBlazorServer.Data
+{
+    public class SimulatedDataProviderService
+    {
+        public List<User> Users { get; set; } = new()
+        {
+            new()
+            {
+                Username = "User1",
+                Password = "User1",
+            },
+            new()
+            {
+                Username = "User2",
+                Password = "User2"
+            },
+            new()
+            {
+                Username = "User3",
+                Password = "User3"
+            }
+        };
+
+        public IEnumerable<string> GetUserRoles(User user)
+        {
+            var result = user.Username switch
+            {
+                "User1" => new List<string>() { "Admin", "User" },
+                "User2" => new List<string>() { "User" },
+                _ => new List<string>() { },
+            };
+
+            return result;
+        }
+    }
+}
